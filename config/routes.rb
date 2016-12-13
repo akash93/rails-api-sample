1dc2344de9f3034a9d7a2220154a54c8a0e1a3da
Akash Khan <akashkhan25@gmail.com>
2016-12-13 16:03:54 +0800

    Added user controller and spec

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json}, path: '/' do
     namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, only: [:show]
 
     end
   end
