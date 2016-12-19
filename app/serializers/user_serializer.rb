65f8274af751207501b3da243bc2bee27ae668bd
Akash Khan <akashkhan25@gmail.com>
2016-12-19 13:54:51 +0800

    Added product link to user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :created_at, :updated_at, :auth_token
+  include Rails.application.routes.url_helpers
+  attributes :id, :email, :created_at, :updated_at, :products
+
+  def products
+    api_v1_user_products_url object.id
+  end
 end
